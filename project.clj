c675a815fc8b37c8c10a2e96b6e8a73f9eb8f1d6
Andrew Moreland <andy@andymo.org>
2011-01-17 15:30:53 -0800

    uppercase groups don't work with clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.Derander/vigilante "1.0.1"
+(defproject org.clojars.derander/vigilante "1.0.1"
   :description "Simple directory watching library"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
